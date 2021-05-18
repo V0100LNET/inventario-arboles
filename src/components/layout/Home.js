d69eb029721eb1c64225f47009ec7e7fe4c961a5
Oswaldo Hernandez <ohernandezv1601@alumno.ipn.mx>
2021-05-18 16:38:24 -0500

    test

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
                 <div className="home__links-link">
                     <a href="/#" className="active">Inicio</a>
                     <a href="/acerca-de">A cerca de</a>
-                    <a href="/contacto">Contacto</a>
+                    <a href="/contacto">Oswaldo</a>
                 </div>
                 <div className="home__links-buton">
                     <button className="register" onClick={registrarUsuario}>Registrarse</button>
